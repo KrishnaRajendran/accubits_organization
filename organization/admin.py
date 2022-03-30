27de062b71977bc258cecdc0cf9e4b2f284464bf
Krishna <krishna.r@accubits.com>
2022-03-30 11:26:14 +0530

    Commit 1

--- organization/admin.py
+++ organization/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from organization.models import Employee, Designation, Department, Team
+from organization.models import Employee, Designation, Department, Team, EmployeeDetail
 
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(Designation)
 admin.site.register(Department)
 admin.site.register(Team)
+admin.site.register(EmployeeDetail)
--- organization/models.py
+++ organization/models.py
@@ -2,35 +2,47 @@ from django.db import models
 
 
 # Create your models here.
-class Department(models.Model):
-    department_name = models.CharField(max_length=100, null=True, unique=True)
+class Employee(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    email = models.EmailField(max_length=120, null=True, unique=True)
+    # team = models.ForeignKey(Team, null=True, on_delete=models.CASCADE)
+    # designation = models.ForeignKey(Designation, null=True, on_delete=models.CASCADE)
+    # department = models.ForeignKey(Department, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.department_name
+        return self.name + ", " + self.email
+        # return self.name
 
 
 class Team(models.Model):
-    team_name = models.CharField(max_length=100, null=True, unique=True)
-    department_name = models.ForeignKey(Department, null=True, on_delete=models.CASCADE)
+    team = models.CharField(max_length=100, null=True, unique=True)
+    members = models.ManyToManyField(Employee)
 
     def __str__(self):
-        return self.team_name
+        return self.team
+
+
+class Department(models.Model):
+    department = models.CharField(max_length=100, null=True, unique=True)
+    teams = models.ManyToManyField(Team)
+
+    def __str__(self):
+        return self.department
 
 
 class Designation(models.Model):
-    employee_designation = models.CharField(max_length=100, null=True, unique=True)
-    team_name = models.ManyToManyField(Team)
+    designation = models.CharField(max_length=100, null=True, unique=True)
 
     def __str__(self):
-        return self.employee_designation
+        return self.designation
 
 
-class Employee(models.Model):
-    employee_name = models.CharField(max_length=50, null=True)
-    email = models.EmailField(max_length=120, null=True, unique=True)
-    department = models.ForeignKey(Department, null=True, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, null=True, on_delete=models.CASCADE)
-    designation = models.ForeignKey(Designation, null=True, on_delete=models.CASCADE)
+class EmployeeDetail(models.Model):
+    name = models.ForeignKey(Employee, null=True, on_delete=models.CASCADE)
+    # email = models.ForeignKey(Employee, null=True, related_name='emailID', on_delete=models.CASCADE)
+    designation = models.ForeignKey(Designation, null=True, on_delete=models.SET_NULL)
+    team = models.ForeignKey(Team, null=True, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, null=True, on_delete=models.PROTECT)
 
     def __str__(self):
-        return self.employee_name
+        return str(self.name)
